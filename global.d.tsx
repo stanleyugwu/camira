d43700b1d2955d328b337b67cdc6d5e04904dd60
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-11-08 10:30:27 +0100

    🧹 chore: add typescript global declarations for image  formats

---
+++ global.d.tsx
@@ -0,0 +1,45 @@
+declare module '*.avif' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.bmp' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.gif' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.jpg' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.jpeg' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.png' {
+  const src: number;
+  export default src;
+}
+
+declare module '*.webp' {
+  const src: number;
+  export default src;
+}
+
+declare module "*.svg" {
+  import * as React from "react";
+
+  const ReactComponent: React.FunctionComponent<
+    React.SVGProps<SVGSVGElement> & { title?: string; onPress?: () => void }
+  >;
+
+  export const src: string;
+  export default ReactComponent;
+}
