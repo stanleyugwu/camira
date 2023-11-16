b97672a84b868eba90f742bb6ba140ed9ad9f05d
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-11-16 16:16:36 +0100

    ⚙ build: update expo app config

--- app.config.ts
+++ app.config.ts
@@ -6,5 +6,28 @@ export default ({ config }: ConfigContext): ExpoConfig => ({
   name: "Camira",
   scheme: ["camira"],
   plugins: ["expo-router"],
+  orientation: "portrait",
+  backgroundColor: "#F0F0FF" /* ThemeColors.primary */,
+  androidNavigationBar: {
+    backgroundColor: "#F0F0FF" /* ThemeColors.primary */,
+    barStyle: "dark-content",
+  },
+  androidStatusBar: {
+    backgroundColor: "#F0F0FF" /* ThemeColors.primary */,
+    barStyle: "dark-content",
+  },
+  userInterfaceStyle: "light",
+  primaryColor: "#500DBC" /* ThemeColors.accent */,
+  developmentClient: {
+    silentLaunch: true,
+  },
+  version: "1.0.0",
+  splash: {
+    backgroundColor: "#500DBC" /* ThemeColors.accent */,
+    resizeMode: "contain",
+  },
   experiments: { tsconfigPaths: true, typedRoutes: true },
+  android: {
+    package: "com.devvie.camira",
+  },
 });
