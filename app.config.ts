456df05cec622bbc20bfde16481921b3d87dc35b
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-11-17 12:52:40 +0100

    ⤴ improvement: split expo configurations into to files `app.json` and `app.config.ts` so that values in `app.json` can be accessed with `expo-constants`

--- app.config.ts
+++ app.config.ts
@@ -1,35 +1,16 @@
 import { ExpoConfig, ConfigContext } from "expo/config";
 
-export default ({ config }: ConfigContext): ExpoConfig => ({
+export default ({ config }: ConfigContext): Partial<ExpoConfig> => ({
   ...config,
-  slug: "camira",
-  name: "Camira",
-  scheme: ["camira"],
-  plugins: ["expo-router"],
-  orientation: "portrait",
-  backgroundColor: "#F0F0FF" /* ThemeColors.primary */,
-  androidNavigationBar: {
-    backgroundColor: "#F0F0FF" /* ThemeColors.primary */,
-    barStyle: "dark-content",
-  },
-  androidStatusBar: {
-    backgroundColor: "#F0F0FF" /* ThemeColors.primary */,
-    barStyle: "dark-content",
-  },
-  userInterfaceStyle: "light",
-  primaryColor: "#500DBC" /* ThemeColors.accent */,
-  developmentClient: {
-    silentLaunch: true,
-  },
-  version: "1.0.0",
-  splash: {
-    backgroundColor: "#500DBC" /* ThemeColors.accent */,
-    resizeMode: "contain",
-  },
-  experiments: { tsconfigPaths: true, typedRoutes: true },
-  android: {
-    package: "com.devvie.camira",
-  },
+  plugins: [
+    "expo-router",
+    [
+      "react-native-vision-camera",
+      {
+        cameraPermissionText: "$(PRODUCT_NAME) needs access to your Camera.",
+      },
+    ],
+  ],
   extra: {
     eas: {
       projectId: "150c8639-91cb-40ae-a4df-f72143d28eed",
