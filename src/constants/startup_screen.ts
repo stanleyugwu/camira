521fee74aa455d8c48fa000ce4cbabf97a16a2dc
Stanley Ugwu <stanleyugwu2018@gmail.com>
2023-11-30 01:43:36 +0100

    ⤴ improvement: create a function to facilitate revers-mapping for `StartupScreen` string enum

--- src/constants/startup_screen.ts
+++ src/constants/startup_screen.ts
@@ -3,12 +3,24 @@
  * text for startup-screen setting
  *
  * The keys are valid screen names that can be routed to, and used in the navigator,
- * while the values are texts that can be shown to user, and stored as setting
+ * while the values are texts that can be shown to user, stored in global state,
+ * and stored as setting
  */
 export enum StartUpScreens {
   "home" = "Home",
   "top_up" = "Airtime Top-Up",
   "scan_document" = "Scan Document",
   "scan_qrcode" = "Scan QR Code or Image",
-  "generate_qrCode" = "Generate QR Code or Image",
+  "generate_qrcode" = "Generate QR Code or Image",
 }
+
+/**
+ * Due to no support string enum reverse mapping,
+ * this function will get the key from the value
+ */
+export const getStartupScreenKeyFromValue = (
+  value: StartUpScreens
+): keyof typeof StartUpScreens =>
+  Object.entries(StartUpScreens).filter(
+    (item) => item[1] === value
+  )[0][0] as keyof typeof StartUpScreens;
